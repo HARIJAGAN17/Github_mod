2a765a3aef12439d24fc7a54f6c213a7ac1945fc
FantasWolf <jaganhari2002@gmail.com>
2024-02-22 19:10:48 +0530

    Tesh stash changed

--- Test.java
+++ Test.java
@@ -5,7 +5,7 @@ class Test{
     public static void main(String[] args)
     {
       Scanner ip = new Scanner(System.in);
-      String test = "Stash";
+      String test = "Stashed--file";
       System.err.println(test);
       ip.close();
     }
