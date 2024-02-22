4d8bc7f65428dfaf3629c4461c41776eed00e8a0
FantasWolf <jaganhari2002@gmail.com>
2024-02-22 21:09:04 +0530

    changed x value

--- Newbranchfile.java
+++ Newbranchfile.java
@@ -1,7 +1,7 @@
 public class Newbranchfile {
     public static void main(String[] args) {
         // ihave created the new branch file
-        int x = 69;
+        int x = 20;
         System.err.println("printing :"+x);
     }
 }
