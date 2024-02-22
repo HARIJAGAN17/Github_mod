1c6c2b8a35938af3d1496fbafc8421c634b6e62c
FantasWolf <jaganhari2002@gmail.com>
2024-02-22 17:45:36 +0530

    changed sum variable

--- Dsa.java
+++ Dsa.java
@@ -5,6 +5,8 @@ public class Dsa {
         int x =3;
         int y = 4;
         int sum = x+y;
+        int subtract = x-y;
+        System.err.println(subtract);
         System.err.println(sum);
         System.out.println(x+y);
     }
