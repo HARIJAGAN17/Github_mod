64fb806ed7eb0a602c6087382f45ba701c745bd2
FantasWolf <jaganhari2002@gmail.com>
2024-02-22 16:50:29 +0530

    test updated

--- Test.java
+++ Test.java
@@ -4,6 +4,9 @@ class Test{
 
     public static void main(String[] args)
     {
-    
+      Scanner ip = new Scanner(System.in);
+      String test = "Stash";
+      System.err.println(test);
+      ip.close();
     }
 }
