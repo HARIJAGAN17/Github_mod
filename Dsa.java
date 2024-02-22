d50fda0f4da71918ca4bbc0fbe02b549ad925c1e
FantasWolf <jaganhari2002@gmail.com>
2024-02-22 19:02:32 +0530

    removed lines

--- Dsa.java
+++ Dsa.java
@@ -2,10 +2,6 @@ import java.util.*;
 public class Dsa {
     public static void main(String[] args) {
         Scanner ip = new Scanner(System.in);
-        int x =3;
-        int z = 4;
-        int sum = x+z;
-
-        System.err.println(sum);
+        
     }
 }
