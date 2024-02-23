e22007b474d4bade5eec67cebb70d2258ff1ad16
FantasWolf <jaganhari2002@gmail.com>
2024-02-23 17:54:29 +0530

    removed files again

--- Dsa.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class Dsa {
-    public static void main(String[] args) {
-        Scanner ip = new Scanner(System.in);
-        System.err.println("commiting new words in dsa file");
-        
-    }
-}
--- Newbranchfile.java
+++
@@ -1,7 +0,0 @@
-public class Newbranchfile {
-    public static void main(String[] args) {
-        // ihave created the new branch file
-        int x = 69;
-        System.err.println("printing :"+x);
-    }
-}
